b58157f57cc5a3997e25c216b411f5a9a3d12ab9
CaferCruz <cafer_cruz@id.uff.br>
2014-08-27 15:08:11 -0300

    Ateração do nome do projeto de Interpolacao para Intercalacao

---
+++ Intercalacao/src/intercalacao/ArvoreDosVencedores.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intercalacao;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Cafer
+ */
+public class ArvoreDosVencedores {
+
+    private No vencedor = null;
+    
+    public ArvoreDosVencedores(ArrayList<No> folhas) {
+        
+    }
+
+    public ArvoreDosVencedores ordenar(ArvoreDosVencedores arvore) {
+        return null;
+    }
+
+    public No getVencedor() {
+        return this.vencedor;
+    }
+
+    public void adicionar(No elem) {
+
+    }
+
+    public void remover(No elem) {
+
+    }
+}
